47856415c6d0da877f0823f077446bffed65c17f
yerx <yer.x.xiong@gmail.com>
2020-07-21 15:19:05 -0400

    added data quality check operator

---
+++ Data-Pipelines-with-Airflow/Project_5/plugins/operators/data_quality.py
@@ -0,0 +1,39 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class DataQualityOperator(BaseOperator):
+
+    ui_color = '#89DA59'
+
+    @apply_defaults
+    def __init__(self,
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # conn_id = your-connection-name
+                 aws_credentials_id="",
+                 redshift_conn_id="",
+                 tables=[],
+                 *args, **kwargs):
+
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        # Map params here
+        # Example:
+        # self.conn_id = conn_id
+        self.aws_credentials_id = aws_credentials_id,
+        self.redshift_conn_id = redshift_conn_id,
+        self.tables = tables
+
+    def execute(self, context):
+      redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+      for table in self.tables:
+        records = redshift.get_records(f"SELECT COUNT(*) FROM {table}")
+        if len(records) < 1 or len(records[0]) < 1:
+          self.log.error(f"Data quality check failed. {table} returned no results")
+          raise ValueError(f"Data quality check failed. {table} returned no results")
+        num_records = records[0][0]
+        if num_records < 1:
+          self.log.error(f"Data quality check failed. No records found in table {table}")
+          raise ValueError(f"Data quality check failed. No records found in table {table}")
+        self.log.info('DataQualityOperator not implemented on table {table}')
